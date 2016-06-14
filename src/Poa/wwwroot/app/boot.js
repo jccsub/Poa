280afa4d2d41c3fc5caf446a4e9cd40d3845cc46
jccsub <chad.chandler@outlook.com>
2016-06-14 15:03:00 -0400

    Revert "Add project files."
    This reverts commit 7473bbb65ce056c07fb68fa8132b370d00042ac9.

--- src/Poa/wwwroot/app/boot.js
+++
@@ -1,2 +0,0 @@
-System.register(["angular2/platform/browser","./app.component"],function(a,b){"use strict";var c,d;b&&b.id;return{setters:[function(a){c=a},function(a){d=a}],execute:function(){c.bootstrap(d.AppComponent)}}});
-//# sourceMappingURL=boot.js.map
