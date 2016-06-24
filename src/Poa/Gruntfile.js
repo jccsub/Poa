18150f8263cde865918560e4172914b8afe1da1a
jccsub <chad.chandler@outlook.com>
2016-06-23 20:22:15 -0400

    Upgraded to angular 2 rc1 (@angular)

--- src/Poa/Gruntfile.js
+++ src/Poa/Gruntfile.js
@@ -37,10 +37,10 @@
 						'Scripts/js/**/*.js',
 						'node_modules/es6-shim/es6-shim.min.js',
 						'node_modules/systemjs/dist/system-polyfills.js',
-						'node_modules/angular2/bundles/angular2-polyfills.js',
+						'node_modules/@angular/bundles/angular2-polyfills.js',
 						'node_modules/systemjs/dist/system.src.js',
 						'node_modules/rxjs/bundles/Rx.js',
-						'node_modules/angular2/bundles/angular2.dev.js'
+						'node_modules/@angular/bundles/angular2.dev.js'
 					],
 					dest: 'wwwroot/js/',
 					filter: 'isFile'
